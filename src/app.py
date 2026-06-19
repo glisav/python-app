5ce7ae037ce6fcbbab7e770b8f9200ad19d8a047
Glisav Katroshi <glisav.katroshi@lhind.dlh.de>
2026-06-19 14:28:14 +0200

    Adds CI Github workflow

--- src/app.py
+++ src/app.py
@@ -8,7 +8,8 @@ app = Flask(__name__)
 def details():
     return jsonify({
         'time': datetime.datetime.now().strftime("%I:%M%p on %B %d, %Y"),
-        'hostname': socket.gethostname()
+        'hostname': socket.gethostname(),
+        'message': 'Hello from Flask!'
     })
 
 @app.route('/api/v1/healthz')
