0b26dfd5a2bae306a3c185156ba5c386ac35e51a
glisav <glisav.katroshi@gmail.com>
2026-06-22 14:00:45 +0200

    Adds argocd installation fix

--- src/app.py
+++ src/app.py
@@ -9,7 +9,7 @@ def details():
     return jsonify({
         'time': datetime.datetime.now().strftime("%I:%M%p on %B %d, %Y"),
         'hostname': socket.gethostname(),
-        'message': 'Hello from Flask, human!!'
+        'message': 'Hello from Flask, human!!!'
     })
 
 @app.route('/api/v1/healthz')
